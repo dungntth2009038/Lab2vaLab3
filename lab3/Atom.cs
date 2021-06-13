84b2ed0c0f22dd0f935fcfa740aee8079b20c917
dung <dung@gmail.com>
2021-06-13 21:06:05 +0700

    upload

---
+++ lab3/Atom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab3
+{
+    public class Atom
+    {
+        public int Number { get; set; }
+        public string Symbol { get; set; }
+        public string FullName { get; set; }
+        public float Weight { get; set; }
+
+        public bool Accept()
+        {
+            try
+            {
+                Console.WriteLine("Enter atomic number: ");
+                this.Number = Convert.ToInt32(Console.ReadLine());
+                if (this.Number == 0)
+                {
+                    return false;   
+                }
+                Console.WriteLine("Enter Symbol: ");
+                this.Symbol = Console.ReadLine();
+                Console.WriteLine("Enter Full name :");
+                this.FullName = Console.ReadLine();
+                Console.WriteLine("Enter atomic weight");
+                this.Weight = float.Parse(Console.ReadLine());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return false;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{this.Number} {this.Symbol} {this.FullName} {this.Weight}");
+        }
+
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Atom atom = new Atom();
+            while (true)
+            {
+                atom.Accept();
+                Console.WriteLine("Chon 1 tiep tuc chon 2 de dung");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice != 1)
+                {
+                    break;
+                }
+            }
+            atom.Display();
+        }
+    }
+}
